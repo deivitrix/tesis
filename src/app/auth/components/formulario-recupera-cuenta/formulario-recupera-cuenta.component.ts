85d2b1df355fb1454ad6d67c9f989e18fec09e95
deivitrix <davitrix001@gmail.com>
2022-01-09 00:01:15 -0500

    mensajes de validacion

--- src/app/auth/components/formulario-recupera-cuenta/formulario-recupera-cuenta.component.ts
+++ src/app/auth/components/formulario-recupera-cuenta/formulario-recupera-cuenta.component.ts
@@ -1,5 +1,7 @@
 import { PathImagenesService } from 'src/app/services/path-imagenes.service';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MensajeLoginComponent } from '../mensaje-login/mensaje-login.component';
 
 @Component({
   selector: 'app-formulario-recupera-cuenta',
@@ -9,11 +11,55 @@ import { Component, OnInit } from '@angular/core';
 export class FormularioRecuperaCuentaComponent implements OnInit {
   correo_usuario="";
   pathrecuperar="";
-  constructor(private _path:PathImagenesService) { 
+  constructor(private _path:PathImagenesService, public snackBar:MatSnackBar) { 
     this.pathrecuperar=this._path.pathrecuperarcorreo;
   }
 
   ngOnInit(): void {
   }
+  Enviarenlace(){
+
+    const reques=this.validateEmail(this.correo_usuario);
+    if(this.correo_usuario.length==0)
+    {
+      this.snackBar.openFromComponent(MensajeLoginComponent,{
+        data:{
+          titulo:'Error.....',
+          mensaje:'Datos Faltantes',
+         buttonText:'',
+         icon:'warning'
+        },
+        duration:1000,
+        horizontalPosition:'end',
+        verticalPosition:'bottom',
+        panelClass:'error'     
+      });
+      return;
+
+    }
+
+    if(reques==false)
+    {
+      this.snackBar.openFromComponent(MensajeLoginComponent,{
+        data:{
+          titulo:'Error.....',
+          mensaje:'Correo electronico invalido',
+         buttonText:'',
+         icon:'warning'
+        },
+        duration:1000,
+        horizontalPosition:'end',
+        verticalPosition:'bottom',
+        panelClass:'error'     
+      });
+      return;
+
+    }
+  }
+
+  validateEmail(email:string) {
+    const regularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regularExpression.test(String(email).toLowerCase());
+   }
 
 }
