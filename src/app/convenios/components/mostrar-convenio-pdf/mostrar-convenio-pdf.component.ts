bcb2ebfa8c6d9b6aa9f2bd59d20e098cdec687b4
deivitrix <davitrix001@gmail.com>
2022-01-22 15:50:55 -0500

    mostrar pdf

--- src/app/convenios/components/mostrar-convenio-pdf/mostrar-convenio-pdf.component.ts
+++ src/app/convenios/components/mostrar-convenio-pdf/mostrar-convenio-pdf.component.ts
@@ -2,6 +2,7 @@ import { ConveniosServicesService } from './../../../services/generalConvenios/c
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import { TablaConvenioModel } from 'src/app/models/convenios/tabla_convenios'
 
 @Component({
   selector: 'app-mostrar-convenio-pdf',
@@ -28,6 +29,10 @@ export class MostrarConvenioPDFComponent implements OnInit {
 
    //variable PDF
    pdfconvenio:SafeResourceUrl;
+
+   ///lista convenio
+   listaCon:TablaConvenioModel[]=[];
+   datoAprobado:TablaConvenioModel={PDF:"",id:0,estado:"",f_creaciondoc:"",femisor_id:0,freceptor_id:0,tipo_documento:"",usuario_id:0,titulo_convenio:""};
 
    cambio_u="http://3.15.185.2/Contenido/ConveniosGuardados/AcuerdosGuardados/ACUERDO%20DE%20COLABORACI%C3%93N%20ENTRE%20LA%20UNIVERSIDAD%20DE%20KANSAS%20Y%20LA%20UTM.pdf";
 
@@ -62,11 +67,13 @@ export class MostrarConvenioPDFComponent implements OnInit {
   getconvenio(){
     this.convenios.serachconveniotabla(this.id)
     .subscribe((res:any)=>{
-      this.datosAprobados=res;
+      this.datoAprobado=res.convenio;
       this.loading=false;
+      this.listaCon=[];
+      this.listaCon.push(this.datoAprobado);
 
-      this.pdfconvenio=this.url.bypassSecurityTrustResourceUrl(this.datosAprobados.convenio.PDF);
-      console.log(this.pdfconvenio);
+      //this.pdfconvenio=this.url.bypassSecurityTrustResourceUrl(this.datosAprobados.convenio.PDF);
+      console.log(this.datoAprobado);
     });
 
   }
---
+++ src/app/models/convenios/tabla_convenios.ts
@@ -0,0 +1,11 @@
+export interface TablaConvenioModel{
+    id:number;
+    usuario_id:number;
+    femisor_id:number;
+    freceptor_id:number;
+    titulo_convenio:string;
+    f_creaciondoc:string;
+    estado:string;
+    tipo_documento:string;
+    PDF:string;
+}
