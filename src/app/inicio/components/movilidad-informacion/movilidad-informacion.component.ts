38fd8b646bb2c01df390a55936099d9a8ce2a4db
david <davitrix001@gmail.com>
2022-03-14 16:09:19 -0500

    movilidad

--- src/app/inicio/components/movilidad-informacion/movilidad-informacion.component.ts
+++ src/app/inicio/components/movilidad-informacion/movilidad-informacion.component.ts
@@ -1,5 +1,7 @@
 import { PathImagenesService } from 'src/app/services/path-imagenes.service';
 import { Component, OnInit } from '@angular/core';
+import { GeneralService } from 'src/app/services/generalget/general.service';
+import { Interfaz_contenido } from 'src/app/models/Interfaz_contenido.model';
 
 @Component({
   selector: 'app-movilidad-informacion',
@@ -8,13 +10,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovilidadInformacionComponent implements OnInit {
   pathmovilidad:string;
+  loading=true;
+  listainterfaz:Interfaz_contenido[]=[];
+  listaobjetivo:Interfaz_contenido[]=[];
+  listaprograma:Interfaz_contenido[]=[];
+  listabeneficios:Interfaz_contenido[]=[];
+  listareglamento:Interfaz_contenido[]=[];
 
-  constructor(private path:PathImagenesService) { 
+  constructor(private path:PathImagenesService, private _general:GeneralService) { 
     this.pathmovilidad=path.pathimagenmovilidad;
-    
+
   }
 
   ngOnInit(): void {
+    this.getPaginas();
   }
 
+  getPaginas(){
+    this._general.getTipoPagina("Movilidad")
+    .subscribe((res:any) => {
+      this.listainterfaz=res;
+      this.loading=false;
+      this.separarObjetivo(this.listainterfaz);
+      this.separarBeneficios(this.listainterfaz);
+     
+    })
+  }
+
+  separarObjetivo(original:Interfaz_contenido[])
+  {
+    original.forEach((item:Interfaz_contenido)=>{
+      if(item.interfaz.nombre=="Objetivo-Programa"){
+         if(item.nombre=="Objetivo General")
+         {
+           if(item.estado=="A"){
+            this.listaobjetivo.push(item);
+           }
+         }
+         if(item.nombre=="Programa de movilidad academica")
+         {
+          if(item.estado=="A"){ 
+            this.listaprograma.push(item);
+          }
+         }
+      }
+    });
+  }
+
+  separarBeneficios(original:Interfaz_contenido[])
+  {
+    original.forEach((item:Interfaz_contenido)=>{
+      if(item.interfaz.nombre=="Beneficios-Reglamento"){
+    if(item.nombre=="Beneficios")
+    {
+      if(item.estado=="A"){
+        this.listabeneficios.push(item)
+      }
+    }
+    if(item.nombre=="Reglamento")
+    {
+      if(item.estado=="A"){
+        this.listareglamento.push(item)
+      
+      }
+    }
+    
+  }
+    });
+  }
 }
